993c4f3f42135701406d3f3739373694cb6b9509
brokenAvocado <ericyc35@gmail.com>
2023-05-17 22:13:57 -0400

    for the pres

--- wall_follower_hardware.py
+++ wall_follower_hardware.py
@@ -1,5 +1,5 @@
 """
-    Group 6: Wall Follower Code for Simulation
+    Group 6: Wall Follower Code for Real World Maze
     Uses a wall following algorithm in order to solve complex mazes
 """
 
@@ -26,7 +26,7 @@ class Follow(Node):
         )
         timer_period = 0.5  # seconds
 
-        # Distance variables saved within class
+        # Distance Variables
         self.min_front_dist = 0
         self.min_frontright_dist = 0
         self.min_backright_dist = 0
@@ -54,26 +54,27 @@ class Follow(Node):
         Sends movement commands to /cmd_vel based on LIDAR readings
         '''
         msg = Twist()
+        # key = self.getch()
 
         ########### Gazebo vs Real World #################
         # comment out the one not in use
 
         # Real World
-        realfactor = 0.14
-        bound1 = 0.38-realfactor       # Side wall close threshold       
-        bound2 = 0.34-realfactor       # Side wall too close threshold     
-        bound3 = 0.38-realfactor       # Front wall threshold              
-        bound4 = 0.40-realfactor       # Side wall far threshold          
-        bound5 = 0.45-realfactor       # Back wall threshold               
-        bound6 = 0.43-realfactor       # Side wall too far threshold 
+        # realfactor = 0.14
+        # bound1 = 0.38-realfactor       # Side wall close threshold       
+        # bound2 = 0.34-realfactor       # Side wall too close threshold     
+        # bound3 = 0.38-realfactor       # Front wall threshold              
+        # bound4 = 0.40-realfactor       # Side wall far threshold          
+        # bound5 = 0.45-realfactor       # Back wall threshold               
+        # bound6 = 0.43-realfactor       # Side wall too far threshold 
 
         # Gazebo
-        # bound1 = 0.38       # Side wall close threshold       
-        # bound2 = 0.34       # Side wall too close threshold     
-        # bound3 = 0.38       # Front wall threshold              
-        # bound4 = 0.40       # Side wall far threshold          
-        # bound5 = 0.45       # Back wall threshold               
-        # bound6 = 0.43       # Side wall too far threshold  
+        bound1 = 0.38       # Side wall close threshold       
+        bound2 = 0.34       # Side wall too close threshold     
+        bound3 = 0.38       # Front wall threshold              
+        bound4 = 0.40       # Side wall far threshold          
+        bound5 = 0.45       # Back wall threshold               
+        bound6 = 0.43       # Side wall too far threshold  
 
         # bound1 = 0.40       # Side wall close threshold       
         # bound2 = 0.38       # Side wall too close threshold     
@@ -100,7 +101,7 @@ class Follow(Node):
             else:
                 msg.linear.x = fast_linear+0.03
                 msg.angular.z = -(med_angular+0.3)
-                self.get_logger().info("special-special right")
+                self.get_logger().info("special right hehe")
         # Path Turning
         elif self.min_frontright_dist < bound1 and self.min_front_dist > bound3:
             self.inTurn = False
@@ -138,7 +139,7 @@ class Follow(Node):
     def listener_callback(self,msg):
         '''
         Subscription Callback 
-        TODO: implement
+        Assigns distances from LIDAR readings to the distance variables intialized with the class declaration
         '''
         # self.get_logger().info(str(len(msg.ranges)))
 
@@ -146,57 +147,61 @@ class Follow(Node):
         # comment out the one not in use
 
         # Real World
-        front = range(-15,15)
-        back_right = range(507, 549)
-        front_right = range(591,739)
-        front_left = range(49,169)
-        back_left = range(211,253)
-        f = 0
-        fl = 95
-        l = 190
-        r = 570
-        fr = 665
+        # front = range(-15,15)
+        # back_right = range(507, 549)
+        # front_right = range(591,739)
+        # front_left = range(49,169)
+        # back_left = range(211,253)
+        # f = 0
+        # fl = 95
+        # l = 190
+        # r = 570
+        # fr = 665
 
 
         # Gazebo
-        # front = range(-7,7)
-        # back_right = range(240,260)
-        # front_right = range(280,350)       
-        # front_left = range(23,80)
-        # back_left = range(100,120)
-        # f = 0
-        # fl = 45
-        # l = 90
-        # r = 270
-        # fr = 315
+        front = range(-7,7)
+        back_right = range(240,260)
+        front_right = range(280,350)       
+        front_left = range(23,80)
+        back_left = range(100,120)
+        f = 0
+        fl = 45
+        l = 90
+        r = 270
+        fr = 315
 
         ##################################################
 
+        # Directly infront
         self.min_front_dist = msg.ranges[f]
         for i in front:
             if msg.ranges[i] < self.min_front_dist:
                 self.min_front_dist = msg.ranges[i]
 
+        # Approximately northwest of robot
         self.min_frontleft_dist = msg.ranges[fl]
         for i in front_left:
             if msg.ranges[i] < self.min_frontleft_dist:
                 self.min_frontleft_dist = msg.ranges[i]
 
+        # Approximately west of robot
         self.min_backleft_dist = msg.ranges[l]
         for i in back_left:
             if msg.ranges[i] < self.min_backleft_dist:
                 self.min_backleft_dist = msg.ranges[i]
 
+        # Approximately east of robot
         self.min_backright_dist = msg.ranges[r]
         for i in back_right:
             if msg.ranges[i] < self.min_backright_dist:
                 self.min_backright_dist = msg.ranges[i]
 
+        # Approximately north east of robot
         self.min_frontright_dist = msg.ranges[fr]
         for i in front_right:
             if msg.ranges[i] < self.min_frontright_dist:
                 self.min_frontright_dist = msg.ranges[i]
-
 
     def shutdown_cmd(self):
         msg = Twist()
@@ -216,5 +221,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
